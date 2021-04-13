cc8cc37a24b031cc2275d2f4e6f30cffcb60cdd2
Michael Adams <mikelovesbooks@gmail.com>
2021-04-13 15:54:43 +0100

    even more typos

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,7 +1,7 @@
 
 from typing import NamedTuple
 
-# We might need to populate actual xt_cells, but that's a bit more
+# We might need to populate actual xy_cells, but that's a bit more
 # than we need to get into until we have to.
 class MockXYCell(NamedTuple):
     value: str

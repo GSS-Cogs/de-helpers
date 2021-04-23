e2aa04378da02d350501a009eca1f7922fc81072
Osamede Asemota <osamede.asemota@gsscogs.uk>
2021-04-23 16:49:07 +0100

    Unit test updated to include more data types.

--- tests/filters/test_cdid.py
+++ tests/filters/test_cdid.py
@@ -37,10 +37,16 @@ class TestCDIDfilter(unittest.TestCase):
     def test_NonCDID_cells_return_false(self):
 
         mocked_non_cdid_cells = [
-            MockXYCell(value="2014", x=3, y=2),
-            MockXYCell(value="London", x=3, y=3),
+             MockXYCell(value='2014Q2', x=3, y=2),
+            MockXYCell(value="Lo8n", x=3, y=3),
             MockXYCell(value="mcuh", x=3, y=4),
             MockXYCell(value="Exports", x=3, y=5),
+            MockXYCell(value=True, x=3, y=6),
+            MockXYCell(value=19.6, x=3, y=7),
+            MockXYCell(value="", x=2, y=6),
+            MockXYCell(value=2019, x=2, y=7),
+            MockXYCell(value=None, x=2, y=8),
+            MockXYCell(value=2021-0o4-23, x=4, y=1)
         ]
 
         for mock_cell in mocked_non_cdid_cells:

e72c3964c5b620bce3cd7c44125ebb01e34b77ae
Osamede Asemota <osamede.asemota@gsscogs.uk>
2021-05-04 15:17:38 +0100

    unit test updated to include the corrcet assertion for test with a failure

--- tests/filters/test_observation.py
+++ tests/filters/test_observation.py
@@ -1,17 +1,14 @@
-
 import unittest
 
-# from tests.helpers import MockXYCell
-from dehelpers.filters import like_observations
-
+from gssutils import *
 from databaker.framework import *
+from dehelpers.filters import like_observations
 
 class TestObservationsFunction(unittest.TestCase):
     
     # Test that our filter returns what was expected
-    def test_like_observations_success(self):
-        
-        tabs = loadxlstabs("tests/filters/sources/OIC.xls") # load tabs
+    def test_like_observations_success(self):   
+        tabs = loadxlstabs("tests/OIC.xls") # load tabs  tests/OIC.xls
         tab = tabs[2] # Table 1a
         
         actual_observation = like_observations(tab)
@@ -21,17 +18,15 @@ class TestObservationsFunction(unittest.TestCase):
         
         
     def test_like_observations_failure(self):
-        
-        
-        tabs = loadxlstabs("tests/filters/sources/OIC.xls") 
+        tabs = loadxlstabs("tests/OIC.xls") 
         tab = tabs[2] #Table 1a
 
         actual_observation = like_observations(tab)
-        expected_observation = tab.excel_ref("A11").expand(DOWN).is_not_blank() |tab.excel_ref('C11').expand(DOWN).expand(RIGHT).is_not_blank() # year dimension added to observations
-        self.assertEqual(actual_observation, expected_observation)
-
+        expected_observation = tab.excel_ref("A11").expand(DOWN).is_not_blank() | tab.excel_ref('C11').expand(DOWN).expand(RIGHT).is_not_blank() # year dimension added to observations
+        self.assertIsNot(actual_observation, expected_observation)
+        
     def test_like_observations_exception(self):
-        tabs = loadxlstabs("tests/filters/sources/OIC.xls") 
+        tabs = loadxlstabs("tests/OIC.xls") 
         tab = tabs[1] # Contents
         
         with self.assertRaises(TypeError) as exception_context:

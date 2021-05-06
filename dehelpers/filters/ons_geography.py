417ee1dc8df17dd0f93131face98520282201c46
Osamede Asemota <osamede.asemota@gsscogs.uk>
2021-05-06 17:46:11 +0100

    removed commented out codes from basic ons geography fiter

--- dehelpers/filters/ons_geography.py
+++ dehelpers/filters/ons_geography.py
@@ -1,6 +1,3 @@
-# def like_ons_geography(xycell):
-#     try:
-#         if isinstance(xycell.value, str) and xycell.value.isalnum() == True and xycell.value.isupper() == 
 def like_ons_geography(xycell):
     if isinstance(xycell.value, str) and xycell.value.isalnum() == True and xycell.value.isupper() == True and len(xycell.value) == 9:
         return True

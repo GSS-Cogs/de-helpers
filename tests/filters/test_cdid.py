7b9a61c001c0967698114b1db0ecdc3b0118fdd4
Osamede Asemota <osamede.asemota@gsscogs.uk>
2021-05-13 11:22:29 +0100

    test updated to ensur filter returns False when expected toe

--- tests/filters/test_cdid.py
+++ tests/filters/test_cdid.py
@@ -5,7 +5,7 @@ from dehelpers.filters import like_cdid
 
 class TestCDIDfilter(unittest.TestCase):
 
-    # Test that our filter returns what was expected
+    # Test that filter returns what was expected
     def test_cdid_cells_return_true(self):
 
         # Create some test data
@@ -18,20 +18,21 @@ class TestCDIDfilter(unittest.TestCase):
         for mock_cell in mocked_cdid_cells:
             self.assertEqual(like_cdid(mock_cell), True)
             
-    # Test that our filter returns False when expected to
+    # Test that filter returns False when expected to
     def test_non_cdid_cells_return_false(self):
 
         mocked_non_cdid_cells = [
             MockXYCell(value='2014Q2', x=3, y=2),
             MockXYCell(value="Lo8n", x=3, y=3),
-            MockXYCell(value="mcuh", x=3, y=4),
-            MockXYCell(value="Exports", x=3, y=5),
+            MockXYCell(value="m3uh", x=3, y=4),
+            MockXYCell(value="N1hn", x=3, y=5),
             MockXYCell(value=True, x=3, y=6),
             MockXYCell(value=19.6, x=3, y=7),
             MockXYCell(value="", x=2, y=6),
             MockXYCell(value=2019, x=2, y=7),
             MockXYCell(value=None, x=2, y=8),
-            MockXYCell(value=2021-0o4-23, x=4, y=1)
+            MockXYCell(value='MUC', x=4, y=1),
+            MockXYCell(value='Mu3H', x=4, y=2)
         ]
 
         for mock_cell in mocked_non_cdid_cells:

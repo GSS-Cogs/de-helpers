1d454a2901ef465b52971277f2645358f1b8fce1
Osamede Asemota <osamede.asemota@gsscogs.uk>
2021-05-21 12:05:51 +0100

    filter revised to return False when expected

--- dehelpers/filters/year_quarter.py
+++ dehelpers/filters/year_quarter.py
@@ -1,11 +1,11 @@
 def like_year_quarter(xycell): 
     if isinstance(xycell.value, (float, int)) and len(str(int(xycell.value))) == 4 and str(xycell.value).startswith(('199', '200', '201', '202')) and int(xycell.value) < 2025:
         return True
-    if isinstance(xycell.value, str) and len(str(xycell.value)) == 4 and str(xycell.value).startswith(('199', '200', '201', '202')) and int(xycell.value) < 2025:
+    elif isinstance(xycell.value, str) and len(str(xycell.value)) == 4 and str(xycell.value).startswith(('199', '200', '201', '202')) and (xycell.value).isnumeric() and int(xycell.value) < 2025:
         return True
-    elif isinstance(xycell.value, str) and len(str(xycell.value)) > 6 and " " in str(xycell.value) and len(str(xycell.value).replace(" ", "")) == 6 and str(xycell.value).startswith(('199', '200', '201', '202')) and str(xycell.value)[-2:] in ['Q1', 'Q2', 'Q3', 'Q4']:
+    elif isinstance(xycell.value, str) and len(str(xycell.value)) > 6 and " " in str(xycell.value) and len(str(xycell.value).replace(" ", "")) == 6 and str(xycell.value)[:4].isnumeric() and str(xycell.value).startswith(('199', '200', '201', '202')) and int(str(xycell.value)[:4]) < 2025 and str(xycell.value)[-2:] in ['Q1', 'Q2', 'Q3', 'Q4']:
         return True
-    elif isinstance(xycell.value, str) and " " not in str(xycell.value) and len(str(xycell.value)) == 6 and str(xycell.value).startswith(('199', '200', '201', '202')) and str(xycell.value)[-2:] in ['Q1', 'Q2', 'Q3', 'Q4']:
+    elif isinstance(xycell.value, str) and " " not in str(xycell.value) and len(str(xycell.value)) == 6 and str(xycell.value)[:4].isnumeric() and str(xycell.value).startswith(('199', '200', '201', '202')) and int(str(xycell.value)[:4]) < 2025 and str(xycell.value)[-2:] in ['Q1', 'Q2', 'Q3', 'Q4']:
         return True
     else:
         return False 
